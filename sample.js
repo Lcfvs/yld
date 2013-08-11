f6bd7a1d23e2b80c536d097040103ed9b6a54bf9
Lcf.vs <lcf.vs@hotmail.com>
2013-08-11 21:11:52 +0200

    Update sample.js
    Optional named arguments dependency removed

--- sample.js
+++ sample.js
@@ -1,33 +1,122 @@
-var asyncFn, closure;
+var yld;
  
-asyncFn = yld(function (closure, a, b) {
-    var child, response;
+yld = (function () {
+    var defer, prepare, yld;
     
-    if (isNaN(a) || isNaN(b)) {
-        // kills the process immediately
-        this.error = 'Invalid values';
-    }
+    defer = typeof process === 'object' && typeof process.nextTick === 'function' ? process.nextTick : function nextTick(closure) {
+        setTimeout(closure);
+    };
+ 
+    prepare = function (fn, container, parent) {
+        var proto, generator, fnGenerator, response;
+        
+        proto = {
+            yld: function (fn) {
+                var container, parent;
+                
+                container = {};
+                parent = this;
+                
+                return function () {
+                    var generator;
+                    
+                    generator = prepare(fn, container, parent);
+                    container.generator = generator;
+                    
+                    return generator.next().apply(null, arguments);
+                };
+            },
+            send: function () {
+                var args;
+                
+                args = arguments;
+                
+                defer(function () {
+                    var error;
+                    
+                    try {
+                        generator.send.apply(generator, args);
+                    } catch (error if error instanceof StopIteration) {}
+                });
+            },
+            set error(value) {
+                var error;
+                
+                error = new Error(value);
+                
+                throw {
+                    name: 'YldError',
+                    message: value,
+                    toString: function () {
+                        var stack;
+                        
+                        stack = error.stack;
+                        
+                        return this.message + '\n\nStack trace:\n\n' + stack.substring(stack.indexOf('\n') + 1);
+                    }
+                };
+            }
+        };
+        
+        if (parent !== undefined) {
+            proto.parent = {
+               send: parent.send,
+               yld: parent.yld
+            };
+            
+            Object.freeze(proto.parent);
+        }
+        
+        Object.freeze(proto);
+        
+        yield function () {
+            generator = container.generator;
+            fnGenerator = fn.apply(proto, arguments);
+            generator.next();
+        };
+        
+        response = yield defer(function () {
+            var error;
+            
+            try {
+                fnGenerator.next();
+            } catch (error if error instanceof StopIteration) {}
+        });
+        
+        while (true) {
+            response = yield defer(function () {
+                var error;
+                
+                try {
+                    fnGenerator.send.apply(fnGenerator, [response]);
+                } catch (error if error instanceof StopIteration) {}
+            });
+        }
+        
+        defer(function () {
+            fnGenerator.close();
+            generator.close();
+        });
+    };
     
-    // this.yld is usable as yld
-    child = yield this.yld(closure)(a);
+    yld = function (fn) {
+        var container;
+        
+        container = {};
+        
+        return function () {
+            var generator;
+            
+            generator = prepare(fn, container);
+            container.generator = generator;
+            
+            return generator.next().apply(null, arguments);
+        };
+    };
     
-    response = yield child.send(b);
-    
-    console.log(response); // 3
-});
+    return yld;
+}());
 
-closure = function (a) {
-    var b, parent;
-    
-    parent = this.parent;
-    
-    // returns this scope to the parent scope
-    b = yield parent.send(this);
-    
-    // returns the response to the parent scope
-    yield setTimeout(function () {
-        parent.send(a + b);
-    }, 3000);
-};
-
-asyncFn(closure, 1, 2);
+if (typeof module === 'object' && module.exports !== undefined) {
+    module.exports = yld;
+}
