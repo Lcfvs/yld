2d80e13678e8624f22e843c9bd25a560c8c0bef9
Lcf.vs <lcf.vs@hotmail.com>
2013-08-14 13:40:06 +0200

    Create how-it-works-1.js

---
+++ examples/how-it-works-1.js
@@ -0,0 +1,12 @@
+var fct, yielded;
+ 
+fct = function fct(value) {
+    yield console.log(value + 1);
+    
+    // never reached, because nothing specifies to jump to the next yield
+    yield console.log(value + 2);
+};
+
+yielded = yld(fct);
+
+yielded(1);
