f65d3b03d9a4ac70d7025af01503fc16300bd966
Lcf.vs <lcf.vs@hotmail.com>
2013-08-14 13:45:20 +0200

    Create how-it-works-2.js

---
+++ examples/how-it-works-2.js
@@ -0,0 +1,17 @@
+var fct, yielded;
+ 
+var fct;
+ 
+fct = function fct(value) {
+    var newValue;
+    
+    newValue = value + 1;
+    console.log(newValue);
+    yield this.send(); // jumps to the next yield
+   
+    yield console.log(newValue + 1); // reached
+};
+
+yielded = yld(fct);
+
+yielded(1);
