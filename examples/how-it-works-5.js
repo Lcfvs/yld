5069c39d5fa56e0038c05c6ab39c007a161e07f1
Lcf.vs <lcf.vs@hotmail.com>
2013-08-14 13:58:42 +0200

    Create how-it-works-5.js

---
+++ examples/how-it-works-5.js
@@ -0,0 +1,19 @@
+var fct;
+ 
+fct = function fct(value) {
+    var self, response;
+ 
+    self = this;
+    response = value; // stores this context in a variable
+ 
+    while (response !== 5) { // conditional yielded loop
+        response = yield setTimeout(function () {
+            var newValue;
+            
+            newValue = response + 1;
+            console.log(newValue);
+            
+            self.send(newValue); // sends the new value to the next context yield
+        }, 2000);
+    }
+};
