30797c2c6fdcdfd53b17223a64ffdd50ac75bda1
Lcf.vs <lcf.vs@hotmail.com>
2013-08-20 11:52:10 +0200

    Update goto.js

--- examples/goto.js
+++ examples/goto.js
@@ -9,14 +9,17 @@ goto = function* () {
                 console.log('started');
                 response = yield this.next('label_0');
             break;
+            
             case 'label_0':
                 console.log('label_0');
                 response = yield this.next('label_2');
             break;
+            
             case 'label_1':
                 console.log('label_1');
                 response = yield this.next(new Error());
             break;
+            
             case 'label_2':
                 console.log('label_2');
                 response = yield this.next('label_1');
