a857f69294d69cb17529d8cf3222711f6b304e04
Lcf.vs <lcf.vs@hotmail.com>
2013-08-20 11:30:15 +0200

    Create goto.js

---
+++ examples/goto.js
@@ -0,0 +1,28 @@
+var goto;
+
+goto = function* () {
+    var response;
+
+    while (!(response instanceof Error)) {
+        switch (response) {
+            case undefined:
+                console.log('started');
+                response = yield this.next('label_0');
+            break;
+            case 'label_0':
+                console.log('label_0');
+                response = yield this.next('label_2');
+            break;
+            case 'label_1':
+                console.log('label_1');
+                response = yield this.next(new Error());
+            break;
+            case 'label_2':
+                console.log('label_2');
+                response = yield this.next('label_1');
+            break;
+        }
+    }
+};
+
+yld(goto)();
