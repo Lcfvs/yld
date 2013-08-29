1ff2713a70ab35631977e0efdf92cba1234c83c3
Lcf.vs <lcf.vs@hotmail.com>
2013-08-29 22:08:55 +0200

    Update how-it-works-3.js

--- examples/how-it-works-3.js
+++ examples/how-it-works-3.js
@@ -4,7 +4,8 @@ fct = function* fct(value) {
     var response;
  
     response = yield this.next(value + 1); // sends some data to the next yield
-    yield console.log(response + 2);
+    
+    console.log(response + 2);
 };
 
 yielded = yld(fct);
